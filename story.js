4e19734b84ea3ba8601019de896c26aaca2424fa
Glenn Croes <glenncroes@gmail.com>
2012-11-11 16:29:42 +0100

    Bugzzzzzzzz

--- story.js
+++ story.js
@@ -1,1 +1,16 @@
+ var dialog = {};
  
+ dialog.width = 500;
+ dialog.height = 400;
+ 
+ dialog.init = function(){
+ 
+ dialog.gbox = map.svg.append("svg:g")
+				.attr("id", "gbox")
+				.attr("transform","translate("+map.w/2;
+ 
+ dialog.welcome = function(){
+ 
+ }
+ 
+ }
